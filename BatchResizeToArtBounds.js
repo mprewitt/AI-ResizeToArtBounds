ceeb7841f987d8b5cf20525e04ec91dca9d9410d
Michael Prewitt <mprewitt@users.noreply.github.com>
2015-08-06 21:51:02 -0400

    Update BatchResizeToArtBounds.js

--- BatchResizeToArtBounds.js
+++ BatchResizeToArtBounds.js
@@ -1,4 +1,5 @@
 // Batch Resize to Art Bounds
+// by Michael Prewitt, michaelprewitt.com
 
 // INSTALLATION:
 // Install in /Applicatons/[Illustrator folder]/Presets/en_US/Scripts
@@ -11,9 +12,29 @@
 // SOURCES:
 // https://forums.adobe.com/thread/287731 
 // https://forums.adobe.com/thread/939710
+// https://forums.adobe.com/thread/1084760
 
 sourceFolder = Folder.selectDialog();
-filesToProcess = sourceFolder.getFiles("*.ai");
+// filesToProcess = sourceFolder.getFiles("*.ai");
+var TypeList = prompt("Enter file types to act on","ai,eps,pdf","File Types");    
+var reg = new RegExp("\.(" + TypeList.replace(/,/g, '|') + ")$", 'i');    
+var filesToProcess = sourceFolder.getFiles(reg)   
+
+var myBorder = -1;
+var myBorderInput = -1;
+
+while (myBorder < 0 || myBorder > 100 || isNaN(myBorder) ) {
+	myBorderInput = prompt("Size of padding around image, in points (0-100)","20","Padding Size");
+	myBorder = parseInt(myBorderInput);
+}
+
+// Create progress bar
+var win = new Window("palette", "ProgressBar", [300, 150, 750, 280]);   
+win.pnl = win.add("panel", [10, 10, 440, 120], "Script Progress");  
+win.pnl.progBar = win.pnl.add("progressbar", [20, 35, 410, 60], 0, 100);  
+win.pnl.progBarLabel = win.pnl.add("statictext", [20, 20, 320, 35], "0%");  
+win.pnl.fileLabel = win.pnl.add("statictext", [20, 65, 410, 80], "File processed: none");  
+win.show();
 
 // Increment through the files
 for (i=0; i < filesToProcess.length; i++) {
@@ -21,7 +42,6 @@ for (i=0; i < filesToProcess.length; i++) {
 	app.open(filesToProcess[i]);
 
 	var doc = app.activeDocument;
-	var myBorder = 20; // Set to width of border desired, in points
 	var myVisibleBounds = doc.visibleBounds; // Rect, which is an array;
 
 	myVisibleBounds[0] -= myBorder; // left coordinate (use negative values to add artboard)
@@ -31,10 +51,28 @@ for (i=0; i < filesToProcess.length; i++) {
 
 	doc.artboards[0].artboardRect = myVisibleBounds;
 
+	// Update open file info
+    win.pnl.fileLabel.text = "File: " + doc.name;
+    // win.pnl.fileLabel = "File: " + doc.path; // Show full path
+    win.update();
+
 	// Save
 	var mySaveOptions = new IllustratorSaveOptions();
 	mySaveOptions.pdfCompatible = true;
 	doc.saveAs(filesToProcess[i],mySaveOptions);
 
 	doc.close();
+
+	// Update progress bar	
+	win.pnl.progBar.value = parseInt( (i/filesToProcess.length)*100 );
+    win.pnl.progBarLabel.text = win.pnl.progBar.value+"%";
+    win.update();
 }
+
+win.pnl.progBar.value = 100;
+win.pnl.progBarLabel.text = win.pnl.progBar.value+"%";
+win.update();
+alert('Done!');
+
+// Close progress bar
+win.close();
